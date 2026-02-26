c4f390cf6e5943c63e386f2f564e7d5f31e3bf82
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-26 22:52:37 +1100

    updated BookList styling

--- src/BookList.jsx
+++ src/BookList.jsx
@@ -5,14 +5,24 @@ const BookList = ({ books }) => {
   //set if is_avail condition
 
   return (
-    <div>
-      <h2>Book List</h2>
+   <div>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between items-center h-[120px]">
+        <div className="flex items-center gap-2">
+          <div className="flex flex-col items-start justify-center h-full">
+          <h2 className="text-2xl font-semibold text-white ml-8 mt-4 mb-2">Book List</h2>
+          <h3 className="text-xl font-light text-slate-400 ml-8 mt-2 mb-6">Browse through currently available titles</h3>
+          </div>
+        </div>
+      </div>
+    
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
       </div>
     </div>
+  </div>
   );
 };
 
